b33a051e5b2134a318e8d2909148e82908267b87
thomas <thomas.oggier@gmail.com>
2023-11-21 17:23:55 +0100

    runing project + add auth(not tested)

--- wineFinder/app/routes/_index.tsx
+++ wineFinder/app/routes/_index.tsx
@@ -1,41 +1,41 @@
-import type { MetaFunction } from "@remix-run/node";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+import { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import authenticator from "~/services/auth.server";
+import { sessionStorage } from "~/services/session.server";
+
+/**
+ * check the user to see if there is an active session, if not
+ * redirect to login page
+ *
+ * @param param0
+ * @returns
+ */
+export let loader: LoaderFunction = async ({ request }) => {
+  return await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
 };
 
-export default function Index() {
+/**
+ *  handle the logout request
+ *
+ * @param param0
+ */
+export const action: ActionFunction = async ({ request }) => {
+  await authenticator.logout(request, { redirectTo: "/login" });
+};
+
+export default function DashboardPage() {
+  const data = useLoaderData<typeof loader>();
+  console.log(data);
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix Protected Dashboard</h1>
+      <p>{data?.name}   {data?.token}</p>
+      <Form method="post">
+        <button>Log Out</button>
+      </Form>
     </div>
   );
 }
